af6ae98eb684888b33849f96ea413ade1de8147a
FrancescoBiasi <biasifrancesco3@gmail.com>
2022-01-26 20:43:28 +0100

    fix test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        learn react Pippo franco detto l imperatore
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react Ciao Francesco Biasi come staadsaddi adasdd 
+          Learn React
         </a>
       </header>
     </div>
